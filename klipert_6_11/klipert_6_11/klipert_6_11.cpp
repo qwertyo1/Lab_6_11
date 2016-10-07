759624b41902b3c5732c4dfebdcc1a17a7fbc96b
qwertyo1 <klipert1968@yandex.ru>
2016-10-08 00:57:03 +0600

    Добавил функции, требуемые в задании

--- klipert_6_11/klipert_6_11/klipert_6_11.cpp
+++ klipert_6_11/klipert_6_11/klipert_6_11.cpp
@@ -2,5 +2,24 @@
 
 int main()
 {
-    return 0;
+	set t;
+	t.addToSet("huy");
+	t.addToSet("pizda");
+	t.addToSet("djigurda");
+	t.printSet();
+	set t2;
+	t2.addToSet("huynane");
+	t2.addToSet("pizda");
+	t2.addToSet("djigurda");
+	t2.addToSet("djigurdinkaa");
+	t2.printSet();
+	cout << t.checkBelongs("huqy") << endl;
+	set combo;
+	combo = t.intersectionOfSets(t2);
+	combo.printSet();
+	set combo1;
+	combo1 = t.mergeOfSets(t2);
+	combo1.printSet();
+	system("pause");
+	return 0;
 }
